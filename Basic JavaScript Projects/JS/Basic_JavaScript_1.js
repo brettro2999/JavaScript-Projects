be5ce94912ea23bf70c42ef06a8bcec08e39a64d
Brett Romero <brero2999@gmail.com>
2020-03-13 17:44:53 -0600

    First JS Push
    javascript basics push

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+var X = "This is JavaScript";
+window.alert(X);
+
+var P = "I said, \"I'll do it,"+ " again and again!\" excitedly.";
+document.write(P);
+
+var car = "Civic", truck = "Ram", SUV = "4Runner";
+var car = car.fontcolor("red"), truck = truck.fontcolor("purple"), SUV = SUV.fontcolor("red");
+document.write(SUV);
+
+document.write(3+8);

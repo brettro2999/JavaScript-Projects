3670ada7d901931fd96c68a00e0d54226f2cb061
Brett Romero <brero2999@gmail.com>
2020-03-15 16:15:42 -0600

    2nd JS Push
    basic JS 2

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,11 @@
+function yes_function()  {
+    var str = "Yes!";
+    var result = str.fontcolor("blue");
+    document.getElementById("Yes_Text").innerHTML = result;
+}
+
+function myFunction()  {
+    var sentence = "Please press Start";
+    sentence += " at any point in time.";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}

3722cf520234ee8fd749fee4383483a05ac0560d
Brett Romero <brero2999@gmail.com>
2020-03-17 22:33:01 -0600

    JS 5
    JavaScript 5th push

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,39 @@
+document.write(typeof 5);
+
+document.write(4E410);
+
+document.write(-4E310);
+
+document.write(10>2);
+
+document.write(5<1);
+
+console.log(4+5);
+
+document.write(8+"007");
+
+console.log(9<3);
+
+document.write(19==19);
+
+document.write(4==7);
+
+document.write(5===5);
+
+document.write("5"===5);
+
+document.write("five"==="six");
+
+document.write(4>5 || 8>3);
+
+document.write(10>8 || 15>12);
+
+
+
+function not_func()  {
+    document.getElementById("Not").innerHTML = !(6<10);
+}
+
+function not_func2()  {
+    document.getElementById("Nope").innerHTML = !(34<24);
+}
